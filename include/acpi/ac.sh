e21d9fcf37eda86b01f1335c76cf72654dcf76ab
Matthias Kruk <m@m10k.eu>
2021-03-22 09:46:46 +0900

    include/acpi: Add modules for querying battery and PSU state
    This commit adds the acpi/battery and acpi/ac module which can be used
    to query the state of batteries and power supply units in the system
    through the kernel's sysfs ACPI interface.

---
+++ include/acpi/ac.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+__init() {
+	declare -gr __acpi_ac_path="/sys/class/power_supply"
+
+	return 0
+}
+
+acpi_ac_get_state() {
+	local psu
+
+	psu="$1"
+
+	if ! cat "$__acpi_ac_path/$psu/online" 2>/dev/null; then
+		return 1
+	fi
+
+	return 0
+}
