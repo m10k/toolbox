7fb50e1cd63ae5e460b0aed686c5e4e32feda85f
Matthias Kruk <m@m10k.eu>
2021-04-03 11:34:51 +0900

    include/is: Add functions for determining the content of strings
    This commit adds ctype.h-style functions for determining what kind of
    data is stored in strings.

---
+++ include/is.sh
@@ -0,0 +1,77 @@
+#!bin/bash
+
+__init() {
+	return 0
+}
+
+is_digit() {
+	local str
+
+	str="$1"
+
+	if [[ "$str" =~ ^[0-9]+$ ]]; then
+		return 0
+	fi
+
+	return 1
+}
+
+is_hex() {
+	local str
+
+	str="$1"
+
+	if [[ "$str" =~ ^[0-9a-fA-F]+$ ]]; then
+		return 0
+	fi
+
+	return 1
+}
+
+is_upper() {
+	local str
+
+	str="$1"
+
+	if [[ "$str" =~ ^[A-Z]+$ ]]; then
+		return 0
+	fi
+
+	return 1
+}
+
+is_lower() {
+	local str
+
+	str="$1"
+
+	if [[ "$str" =~ ^[a-z]+$ ]]; then
+		return 0
+	fi
+
+	return 1
+}
+
+is_alpha() {
+	local str
+
+	str="$1"
+
+	if is_upper "$str" || is_lower "$str"; then
+		return 0
+	fi
+
+	return 1
+}
+
+is_alnum() {
+	local str
+
+	str="$1"
+
+	if is_alpha "$str" || is_digit "$str"; then
+		return 0
+	fi
+
+	return 1
+}
