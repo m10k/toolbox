28b30415bec97195c214f71eed137adbc49846e8
Matthias Kruk <m@m10k.eu>
2021-06-11 05:43:44 +0900

    COPYING: Add GPLv3 license, copyright headers
    None of the source files currently makes any mention of license terms
    that toolbox is published under. This commit adds the license text of
    the GPLv3 and copyright headers to each of the source files (except
    unit tests).

--- include/sem.sh
+++ include/sem.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# sem.sh - Toolbox module for POSIX-like semaphores
+# Copyright (C) 2021 Matthias Kruk
 #
-# sem - POSIX-like semaphores for bash scripts
-# Copyright (C) 2021 - Matthias Kruk <m@m10k.eu>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 __init() {
 	if ! include "is" "mutex" "wmutex" "log"; then
--- toolbox.sh
+++ toolbox.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+# toolbox.sh - Framework for modular bash scripts
+# Copyright (C) 2021 Matthias Kruk
 #
-# Toolbox - A set of unsophisticated bash "modules"
-# Copyright (C) 2021 - Matthias Kruk <m@m10k.eu>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 __toolbox_init() {
 	local modpath
