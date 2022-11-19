0d22cb2453a7c8306089cee8b39efbabe165f585
Matthias Kruk <m@m10k.eu>
2022-11-19 19:38:00 +0900

    include/queue: Turn queue into a simple FIFO
    The queue module contains functions for file queues and queues with
    unique contents, which should be used into separate modules to keep
    modules simple and avoid mistakes such as using functions for normal
    queues on file-queues.
    
    This commit removes functions for file and unique queues from the
    queue module, turning queues into simple FIFO data structures.

--- include/queue.sh
+++ include/queue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # queue.sh - Toolbox module for "thread"-safe queues
-# Copyright (C) 2021 Matthias Kruk
+# Copyright (C) 2021-2022 Matthias Kruk
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -137,15 +137,6 @@ _queue_get_data() {
 	echo "$path/data"
 }
 
-_queue_get_filedir() {
-	local queue="$1"
-
-	local path
-
-	path=$(_queue_get_path "$queue")
-	echo "$path/files"
-}
-
 queue_init() {
 	local queue="$1"
 
@@ -219,161 +210,6 @@ queue_put() {
 	return "$err"
 }
 
-_queue_contains() {
-	local queue="$1"
-	local item="$2"
-
-	local data
-	local qitem
-
-	data=$(_queue_get_data "$queue")
-
-	if ! [ -f "$data" ]; then
-		return 1
-	fi
-
-	while read -r qitem; do
-		if [[ "$qitem" == "$item" ]]; then
-			return 0
-		fi
-	done < "$data"
-
-	return 1
-}
-
-queue_put_unique() {
-	local queue="$1"
-	local item="$2"
-
-	local mutex
-	local sem
-	local data
-	local err
-
-	# When this function returns success, the caller can be sure that
-	# the item is in the queue. However, this includes the case that
-	# it was already in the queue. Since the item is transient data,
-	# this behavior seems appropriate. Files are a different story.
-
-	mutex=$(_queue_get_mutex "$queue")
-	sem=$(_queue_get_sem "$queue")
-	data=$(_queue_get_data "$queue")
-
-	mutex_lock "$mutex"
-
-	if _queue_contains "$queue" "$item"; then
-		err=-1
-	else
-		if ! echo "$item" >> "$data"; then
-			err=1
-		else
-			err=0
-		fi
-	fi
-
-	mutex_unlock "$mutex"
-
-	if (( err == 0 )); then
-		if ! sem_post "$sem"; then
-			err=1
-		fi
-	elif (( err < 0 )); then
-		err=0
-	fi
-
-	return "$err"
-}
-
-_queue_move_to_q() {
-	local queue="$1"
-	local filepath="$2"
-
-	local filename
-	local filedir
-	local data
-	local dest
-
-	filedir=$(_queue_get_filedir "$queue")
-	data=$(_queue_get_data "$queue")
-	filename="${filepath##*/}"
-	dest="$filedir/$filename"
-
-	if ! cp -a "$filepath" "$dest"; then
-		return 1
-	fi
-
-	if ! echo "$filename" >> "$data"; then
-		log_error "Could not append to queue: $data"
-
-		if ! rm -rf "$dest"; then
-			log_error "Could not remove file from queue: $dest"
-		fi
-
-		return 1
-	fi
-
-	if ! rm -rf "$filepath"; then
-		log_error "Could not remove source file: $filepath"
-	fi
-
-	return 0
-}
-
-queue_put_file() {
-	local queue="$1"
-	local filepath="$2"
-
-	local mutex
-	local sem
-	local filedir
-	local filename
-	local data
-	local err
-
-	# Unlike queue_put_unique(), this function returns failure if the
-	# file was already in the queue. The file queue does not allow
-	# duplicates because files would be overwritten.
-
-	filename="${filepath##*/}"
-
-	mutex=$(_queue_get_mutex "$queue")
-	filedir=$(_queue_get_filedir "$queue")
-	sem=$(_queue_get_sem "$queue")
-
-	mutex_lock "$mutex"
-
-	if ! mkdir -p "$filedir" &> /dev/null; then
-		err=1
-	else
-		local dest
-
-		dest="$filedir/$filename"
-
-		if [ -e "$filedir/$filename" ]; then
-			# Must not succeed if the file was already in the queue
-			err=1
-		else
-			if _queue_move_to_q "$queue" "$filepath"; then
-				err=0
-			else
-				err=1
-			fi
-		fi
-	fi
-
-	mutex_unlock "$mutex"
-
-	if (( err == 0 )); then
-		if ! sem_post "$sem"; then
-			err=1
-		fi
-	elif (( err < 0 )); then
-		err=0
-	fi
-
-	return "$err"
-}
-
 queue_get() {
 	local queue="$1"
 	local -i timeout="$2"
@@ -415,94 +251,6 @@ queue_get() {
 	fi
 
 	echo "$item"
-	return 0
-}
-
-queue_get_file() {
-	local queue="$1"
-	local destdir="$2"
-	local -i timeout="$3"
-
-	local sem
-	local mutex
-	local data
-	local item
-	local dest
-	local err
-
-	if (( $# < 3 )); then
-		timeout=-1
-	fi
-
-	if ! [ -d "$destdir" ]; then
-		log_error "Destination must be a directory"
-		return 1
-	fi
-
-	sem=$(_queue_get_sem "$queue")
-	mutex=$(_queue_get_mutex "$queue")
-	data=$(_queue_get_data "$queue")
-
-	err=false
-
-	if ! sem_wait "$sem" "$timeout"; then
-		return 1
-	fi
-
-	mutex_lock "$mutex"
-
-	if ! item=$(head -n 1 "$data" 2>/dev/null); then
-		err=true
-	else
-		src="$(_queue_get_filedir "$queue")/$item"
-		dest="${destdir%/}/$item"
-
-		# The reason we remove the item from the list first is that
-		# it is much cheaper to put it back in case the move failed
-		# than the other way around.
-
-		if ! sed -i '1d' "$data" 2>/dev/null; then
-			log_error "Could not remove item from $data"
-			err=true
-		else
-			log_debug "Moving $src to $dest"
-
-			if ! mv "$src" "$dest"; then
-				local tmpfile
-				local restored
-
-				log_error "Could not move $src to $dest"
-				restored=true
-
-				# Put the file back to the head of the list
-				if ! tmpfile=$(mktemp); then
-					restored=false
-				elif ! echo "$item" | cat - "$data" > "$tmpfile"; then
-					restored=false
-					rm "$tmpfile"
-				elif ! mv "$tmpfile" "$data"; then
-					restored=false
-					rm "$tmpfile"
-				fi
-
-				if ! "$restored"; then
-					log_error "Could not restore queue $queue"
-				else
-					sem_post "$sem"
-				fi
-
-				err=true
-			fi
-		fi
-	fi
-
-	mutex_unlock "$mutex"
-
-	if "$err"; then
-		return 1
-	fi
-
-	echo "$dest"
 	return 0
 }
 
